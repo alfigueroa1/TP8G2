66cf7d4b0840afc364b394877d7d38460f0c23ef
alfigueroa1 <alfigueroa@itba.edu.ar>
2019-05-19 19:13:50 -0300

    conv2float ahora reconoce numeros sin decimal

--- TP8G2/input.c
+++ TP8G2/input.c
@@ -167,25 +167,7 @@ static float conv2float (char *num){
     int cant = 0;
     int dp = -1;
     float res = 0;
-    if (num[0] == '+'){                         //Si el numero tiene un '+' al inicio, este es obviado
-        while((num[i] != '\0')){                //Se busca donde esta el punto decimal
-            if(num[i] == '.'){
-                dp = i;                         //Y se guarda
-            }
-            i++;
-        }
-        cant = i;                               //Se guarda la cantidad de elementos del arreglo
-        for(i = 1; i < dp; i++){                //Se trabaja con los numeros a la izquierda del decimal
-            res += ((num[i]-48)* (power(dp-i-1)));  //Se los suma uno por uno, multiplicados por su respectiva potencia de 10
-        }
-    
-        //ahora i = dp
-        for(i = dp+1 ; i < cant; i++){          //Se trabaja con los numeros a la derecha del decimal
-            res += ((float)(num[i]-48)/ (power(i-dp)));
-        }
-        return res;                             //Se devuelve el resultado
-    }
-    else if(num[0] == '-'){                 //Si el numero tiene un '-' al inicio, se lo aplica al final
+    if (num[0] == '+'){
         while((num[i] != '\0')){
             if(num[i] == '.'){
                 dp = i;
@@ -193,6 +175,30 @@ static float conv2float (char *num){
             i++;
         }
         cant = i;
+        if(dp == -1){
+            dp = cant;
+        }
+        for(i = 1; i < dp; i++){
+            res += ((num[i]-48)* (power(dp-i-1)));
+        }
+    
+        //ahora i = dp
+        for(i = dp+1 ; i < cant; i++){
+            res += ((float)(num[i]-48)/ (power(i-dp)));
+        }
+        return res;
+    }
+    else if(num[0] == '-'){
+        while((num[i] != '\0')){
+            if(num[i] == '.'){
+                dp = i;
+            }
+            i++;
+        }
+        cant = i;
+        if(dp == -1){
+            dp = cant;
+        }
         for(i = 1; i < dp; i++){
             res += ((num[i]-48)* (power(dp-i-1)));
         }
@@ -203,7 +209,7 @@ static float conv2float (char *num){
         }
         return (res*(-1));
     }
-    else{                                       //Si solo se tiene el numero, no se aplica ningun criterio
+    else{
         while((num[i] != '\0')){
             if(num[i] == '.'){
                 dp = i;
@@ -211,6 +217,9 @@ static float conv2float (char *num){
             i++;
         }
         cant = i;
+        if(dp == -1){
+            dp = cant;
+        }
         for(i = 0; i < dp; i++){
             res += ((num[i]-48)* (power(dp-i-1)));
         }
